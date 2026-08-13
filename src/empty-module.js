26765af584e35b530490196625c100c292a1f647
WriteCodeWithUs <mutepairejonah@gmail.com>
2026-08-13 07:33:38 +0200

    Update empty-module.js

--- src/empty-module.js
+++ src/empty-module.js
@@ -1,11 +1,41 @@
 // src/empty-module.js
-// Empty module to mock Node.js modules in browser environment
-export default {};
-export const AsyncLocalStorage = class AsyncLocalStorage {
+// Mock for Node.js async_hooks in browser environment
+
+export class AsyncLocalStorage {
+  constructor() {
+    this.store = undefined;
+  }
+
   run(store, callback) {
-    return callback();
+    const previousStore = this.store;
+    this.store = store;
+    try {
+      return callback();
+    } finally {
+      this.store = previousStore;
+    }
   }
+
   getStore() {
-    return undefined;
+    return this.store;
   }
+
+  disable() {
+    this.store = undefined;
+  }
+
+  enterWith(store) {
+    this.store = store;
+  }
+}
+
+// Default export for compatibility
+export default {
+  AsyncLocalStorage,
 };
+
+// Mock other common Node.js modules
+export const createHook = () => ({ enable: () => {}, disable: () => {} });
+export const executionAsyncId = () => 0;
+export const triggerAsyncId = () => 0;
+export const executionAsyncResource = () => ({});
